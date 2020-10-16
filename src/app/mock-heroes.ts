3eb1636afc8df128d8f547c75f5aabd13fa1f75f
Medardus2004 <tobim.doe@web.de>
2020-10-16 15:46:52 +0200

    Preise hinzugefügt

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,16 +1,16 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Tschechisch - Deutsch' },
-  { id: 12, name: 'Deutsch - Tschechisch' },
-  { id: 13, name: 'Slowakisch - Deutsch' },
-  { id: 14, name: 'Deutsch - Slowakisch' },
-  { id: 15, name: 'Verhandlungs-Dolmetschen' },
-  { id: 16, name: 'Simultan - Dolmetschen' },
-  { id: 17, name: 'Flüster - Dolmetschen' },
-  { id: 18, name: 'Begleit - Dolmetschen' },
-  { id: 19, name: 'Übersetzung - Arztbriefe' },
-  { id: 20, name: 'Übersetzung - Technische Dokumente' },
-  { id: 21, name: 'Übersetzung - Literatur' },
-  { id: 22, name: 'Übersetzung - Juristische Texte' },
+  { id: 11, name: 'Tschechisch - Deutsch', preis: 100 },
+  { id: 12, name: 'Deutsch - Tschechisch', preis: 200  },
+  { id: 13, name: 'Slowakisch - Deutsch' , preis: 300 },
+  { id: 14, name: 'Deutsch - Slowakisch' , preis: 400 },
+  { id: 15, name: 'Verhandlungs-Dolmetschen' , preis: 500 },
+  { id: 16, name: 'Simultan - Dolmetschen' , preis: 600 },
+  { id: 17, name: 'Flüster - Dolmetschen' , preis: 700 },
+  { id: 18, name: 'Begleit - Dolmetschen' , preis: 800 },
+  { id: 19, name: 'Übersetzung - Arztbriefe' , preis: 900 },
+  { id: 20, name: 'Übersetzung - Technische Dokumente' , preis: 1000 },
+  { id: 21, name: 'Übersetzung - Literatur' , preis: 1100 },
+  { id: 22, name: 'Übersetzung - Juristische Texte' , preis: 1200 },
 ];
