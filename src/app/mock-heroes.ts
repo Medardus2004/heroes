1401382612df388fbd0f56c8c61ec1448df8b9b2
Medardus2004 <tobim.doe@web.de>
2020-10-16 13:56:12 +0200

    vor route aenderung

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -5,7 +5,12 @@ export const HEROES: Hero[] = [
   { id: 12, name: 'Deutsch - Tschechisch' },
   { id: 13, name: 'Slowakisch - Deutsch' },
   { id: 14, name: 'Deutsch - Slowakisch' },
-  { id: 15, name: 'Dolmetschen' },
-  { id: 16, name: 'Simulatan-Dolmetschen' },
-  { id: 17, name: 'Flüster-Dolmetschen' }
+  { id: 15, name: 'Verhandlungs-Dolmetschen' },
+  { id: 16, name: 'Simultan - Dolmetschen' },
+  { id: 17, name: 'Flüster - Dolmetschen' },
+  { id: 18, name: 'Begleit - Dolmetschen' },
+  { id: 19, name: 'Übersetzung - Arztbriefe' },
+  { id: 20, name: 'Übersetzung - Technische Dokumente' },
+  { id: 21, name: 'Übersetzung - Literatur' },
+  { id: 22, name: 'Übersetzung - Juristische Texte' },
 ];
